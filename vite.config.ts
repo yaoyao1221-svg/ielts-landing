d41857623cf271a891aa9e384952e6d6d1259a9b
垚垚1221 <798864201@qq.com>
2026-06-30 14:25:20 +0800

    add built docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/ielts-landing/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react(), tailwindcss()],
 })
